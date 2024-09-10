88cddd16f7be3770cb68919e267984960a911475
Lauri L. <lauri.luhaaar@tktk.ee>
2024-09-10 23:15:20 +0300

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please, enter your age:");
+int userAge = Int32.Parse(Console.ReadLine());
+if (userAge >= 13)
+{
+    Console.WriteLine("Welcome to Instagram!");
+}
+else
+{
+    Console.WriteLine("You are too young to use instagram!");
+
+}
---
+++ Week2/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.Design;
+
+Console.WriteLine("Please select your gender (m/f):");
+char userGender = Char.Parse(Console.ReadLine());
+Console.WriteLine("Please, enter your lastname:");
+string userLastName = Console.ReadLine();
+if (userGender == 'm')
+{
+    Console.WriteLine($"Hello, Mr. {userLastName}!");
+}
+
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Hello, Ms. {userLastName}!");
+}
+
+else
+{
+    Console.WriteLine($"Welcome {userLastName}!");
+}
